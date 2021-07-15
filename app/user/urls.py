39f0bcc48da2e8301364e17a60e54e28efea8616
bangrobe <bangrobe@gmail.com>
2021-07-15 13:55:47 +0700

    Update user token endpoint

---
+++ app/user/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+
+from user import views
+
+app_name = 'user'
+urlpatterns = [
+    # name create co the duoc dung trong reverse url
+    path('create/', views.CreateUserView.as_view(), name='create'),
+    path('token/', views.CreateTokenView.as_view(), name='token')
+]
